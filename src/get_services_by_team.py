9576822696259dbcf50611764166ffcb75280a3b
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-11-26 15:59:45 -0600

    add docstring to _fetch_services_by_team function

--- src/get_services_by_team.py
+++ src/get_services_by_team.py
@@ -3,6 +3,12 @@ import requests
 
 
 def _fetch_services_by_team(team_id: str, api_url: str):
+    """
+    Calls the service atlas api to get services for a team
+    :param team_id:
+    :param api_url:
+    :return:
+    """
     response = requests.get(f"{api_url}/teams/{team_id}/services")
     response.raise_for_status()
     return response.json()
