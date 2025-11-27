a092fded3699e445db05307000dcaf57b88e23c9
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-11-27 10:02:33 -0600

    add _fetch_all_teams function to get all teams from API

--- src/get_services_by_team.py
+++ src/get_services_by_team.py
@@ -12,3 +12,13 @@ def _fetch_services_by_team(team_id: str, api_url: str):
     response = requests.get(f"{api_url}/teams/{team_id}/services")
     response.raise_for_status()
     return response.json()
+
+def _fetch_all_teams(api_url: str) -> list:
+    """
+    Returns all teams from the service atlas api
+    :param api_url:
+    :return:
+    """
+    response = requests.get(f"{api_url}/teams")
+    response.raise_for_status()
+    return response.json()
