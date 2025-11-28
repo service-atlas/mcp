0cb14918b51f48587e74af54f0704185bdf283fc
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-11-27 20:05:24 -0600

    add timeout parameter to API requests and improve docstrings

--- src/get_services_by_team.py
+++ src/get_services_by_team.py
@@ -2,23 +2,23 @@ import requests
 
 
 
-def _fetch_services_by_team(team_id: str, api_url: str):
+def _fetch_services_by_team(team_id: str, api_url: str) -> list:
     """
     Calls the service atlas api to get services for a team
-    :param team_id:
-    :param api_url:
+    :param team_id: the guid for the team
+    :param api_url: the url base for the service atlas api
     :return:
     """
-    response = requests.get(f"{api_url}/teams/{team_id}/services")
+    response = requests.get(f"{api_url}/teams/{team_id}/services", timeout=10)
     response.raise_for_status()
     return response.json()
 
 def _fetch_all_teams(api_url: str) -> list:
     """
     Returns all teams from the service atlas api
-    :param api_url:
+    :param api_url: the url base for the service atlas api
     :return:
     """
-    response = requests.get(f"{api_url}/teams")
+    response = requests.get(f"{api_url}/teams", timeout=10)
     response.raise_for_status()
     return response.json()
