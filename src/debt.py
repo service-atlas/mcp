ea24f7bb6469eb5b8745f39750155cddc8dc9be6
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-12-02 20:20:36 -0600

    Refactor `debt.py` to improve spacing and remove unused imports

--- src/debt.py
+++ src/debt.py
@@ -1,10 +1,9 @@
-import json
-
 from fastmcp import FastMCP
 
 from api_calls import api_caller
 
 debt_mcp = FastMCP("Debt MCP")
+
 
 @debt_mcp.prompt('get_debt')
 def prompt_get_debt() -> str:
@@ -14,17 +13,21 @@ def prompt_get_debt() -> str:
         If you wish to get a list of debts for a specific service, use the tool `get_debts_for_service` passing in the service id or the resource `serviceatlas://debts/{service_id}`.
     """
 
+
 @debt_mcp.tool()
 def get_debt():
     return api_caller.call_get('/reports/services/debt')
+
 
 @debt_mcp.resource(uri='serviceatlas://debts', name='Get Debt Report', mime_type='application/json')
 def get_debts_resource():
     return api_caller.call_get('/reports/services/debt')
 
+
 @debt_mcp.tool()
 def get_debts_for_service(service_id: str):
     return api_caller.call_get(f'/services/{service_id}/debt')
+
 
 @debt_mcp.resource('serviceatlas://debts/{service_id}')
 def get_debts_for_service_resource(service_id: str):
